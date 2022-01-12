0707996c127d5124c5032b68b8a727b5581ff73d
DanaDobkin <dana.dobkin@outlook.com>
2022-01-12 15:51:15 +0200

    Code First Part

---
+++ Book.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace BookLibraryProject
+{
+    class Book
+    {
+        // Data Members
+        private string code;
+
+        // Constructor
+        public Book(string code)
+        {
+            this.code = code;
+        }
+
+        // Methods
+        public bool DoesThisBookExistInLibrary(List<Book> library)
+        {
+            if (library != null)
+            {
+                foreach (Book b in library)
+                {
+                    if (this.code.Equals(b.GetCode()))
+                        return true;
+                }
+            }
+            else
+                Console.WriteLine("This book is null");
+
+            return false;
+        }
+        public bool VerifyBookCode()
+        {
+            if (this.code == null)
+            {
+                return false;
+            }
+            else if (this.code.Length > 6 || this.code.Length < 6)
+            {
+                return false;
+            }
+            else if (!Regex.IsMatch("[A-Z]", this.code)) // couldn't find letters
+            {
+                return false;
+            }
+            else if (!Regex.IsMatch("[1-9]", this.code)) // couldn't find numbers
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // Get Methods
+        public string GetCode()
+        {
+            return this.code;
+        }
+
+
+
+
+
+
+
+
+        //else if (this.code.Count(f => (f == "[A-Z]")) > 2) // more than two letters
+        //{
+        //    return false;
+        //}
+    }
+}
---
+++ Subscriber.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookLibraryProject
+{
+    class Subscriber
+    {
+        // Data Members
+        private string lastName;
+        private int id;
+        private List<Book> books;
+
+        public Subscriber(string lastName, int id)
+        {
+            this.lastName = lastName;
+            this.id = id;
+            
+            books = new List<Book>();
+        }
+
+        // Methods
+        public bool VerifyingID()
+        {
+            if (this.id.ToString().Length != 9)
+            {
+                Console.WriteLine("The ID is incorrect");
+
+                return false;
+            }
+            
+            return true;
+        }
+
+        public void TakeANewBook(string code)
+        {
+            this.books.Add(new Book(code));
+        }
+
+        public void ReturnABook(string code)
+        {
+            this.books.Remove(this.GetASpecificBook(code));
+        }
+
+        public Book GetASpecificBook(string code)
+        {
+            foreach (Book book in this.books)
+            {
+                if (book.GetCode().Equals(code))
+                {
+                    return book;
+                }
+            }
+
+            return null;
+        }
+
+        public bool DoIHaveTheBook(string code)
+        {
+            foreach (Book book in this.books)
+            {
+                if (book.GetCode().Equals(code))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int CountBooks()
+        {
+            return this.books.Count;
+        }
+
+        // Get Methods
+        public string GetLastName()
+        {
+            return this.lastName;
+        }
+
+        public int GetID()
+        {
+            return this.id;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return this.books;
+        }    
+    }
+}
